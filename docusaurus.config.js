08037d11136d91e1dc41b14e5579235a6d03440d
pr3b <eb.underwood22@gmail.com>
2023-06-23 00:02:16 +0700

    mindkey-test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "ramandaprabu", // Usually your GitHub org/user name.
+  organizationName: "pr3b", // Usually your GitHub org/user name.
   projectName: "mindkey.github.io", // Usually your repo name.
   deploymentBranch: "gh-pages",
   trailingSlash: false,
