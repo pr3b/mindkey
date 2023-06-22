94069b4879fcf1a94c60d07e5f61bf677c7a7953
pr3b <eb.underwood22@gmail.com>
2023-06-22 23:33:28 +0700

    mindkey-test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Mindkey",
   tagline: "Technical Documentation",
-  url: "https://pr3b.github.io/",
+  url: "https://mindkey.github.io/",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
